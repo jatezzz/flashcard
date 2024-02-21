9cd3babd0df626197c6bb95963a3ba3c7900a750
jatezzz <johnandrestrujillo@gmail.com>
2024-02-21 17:00:21 -0500

    layout

--- composeApp/src/commonMain/kotlin/screens/list/ScrollablePageScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/list/ScrollablePageScreen.kt
@@ -2,21 +2,25 @@ package screens.list
 
 import QuestionObject
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
@@ -38,19 +42,23 @@ fun ScrollablePageScreen(
     KamelImage(
         resource = asyncPainterResource(data = obj.primaryImageSmall),
         contentDescription = obj.title,
-        contentScale = ContentScale.Crop,
-        modifier = Modifier.fillMaxSize().background(Color.Black).alpha(0.5F),
+        contentScale = ContentScale.Fit,
+        modifier = Modifier.fillMaxSize().background(Color.Black).alpha(0.9F),
     )
 
-
-    // Content Section
-    Box(
+    Row(
         modifier = Modifier
             .fillMaxSize()
-            .padding(8.dp)
+            .padding(top = 50.dp, bottom = 60.dp, start = 8.dp, end = 8.dp)
     ) {
-        Column {
 
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Bottom
+        ) {
             Text(
                 text = obj.artistDisplayName,
                 color = Color.Gray,
@@ -69,31 +77,52 @@ fun ScrollablePageScreen(
                 color = Color.Gray,
                 fontSize = 12.sp
             )
-            Spacer(modifier = Modifier.height(24.dp))
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Favorite,
-                    contentDescription = "Likes",
-                    tint = Color.White
-                )
-                Text(text = "87", color = Color.White, modifier = Modifier.padding(start = 8.dp))
-                Spacer(modifier = Modifier.width(24.dp))
-                Icon(
-                    imageVector = Icons.Default.Email,
-                    contentDescription = "Comments",
-                    tint = Color.White
-                )
-                Text(text = "2", color = Color.White, modifier = Modifier.padding(start = 8.dp))
-                Spacer(modifier = Modifier.width(24.dp))
-                Icon(
-                    imageVector = Icons.Default.Share,
-                    contentDescription = "Shares",
-                    tint = Color.White
-                )
-                Text(text = "203", color = Color.White, modifier = Modifier.padding(start = 8.dp))
-            }
         }
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Icon(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "Account",
+                modifier = Modifier.requiredSize(44.dp),
+                tint = Color.White
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "Likes",
+                modifier = Modifier.requiredSize(40.dp),
+                tint = Color.White
+            )
+            Text(text = "87", color = Color.White)
+            Spacer(modifier = Modifier.height(20.dp))
+            Icon(
+                imageVector = Icons.Default.Email,
+                contentDescription = "Comments",
+                modifier = Modifier.requiredSize(40.dp),
+                tint = Color.White
+            )
+            Text(text = "2", color = Color.White)
+            Spacer(modifier = Modifier.height(20.dp))
+            Icon(
+                imageVector = Icons.Default.List,
+                contentDescription = "Save",
+                modifier = Modifier.requiredSize(40.dp),
+                tint = Color.White
+            )
+            Text(text = "2", color = Color.White)
+            Spacer(modifier = Modifier.height(20.dp))
+            Icon(
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = "Shares",
+                modifier = Modifier.requiredSize(40.dp),
+                tint = Color.White
+            )
+            Text(text = "203", color = Color.White)
+        }
+
     }
 }
