baae165ba8ac7e949e4c48e93f5486d28cc9d6b0
jatezzz <johnandrestrujillo@gmail.com>
2024-02-21 16:27:01 -0500

    Top bar updated.

--- composeApp/src/commonMain/kotlin/screens/list/ListScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/list/ListScreen.kt
@@ -2,17 +2,25 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -23,6 +31,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -30,7 +39,7 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import screens.list.TikTokLikeScreen
+import screens.list.ScrollablePageScreen
 
 
 data object ListScreen : Screen {
@@ -57,7 +66,7 @@ data object ListScreen : Screen {
                     ) {
                         val itemWidth = maxWidth
                         val itemHeight = maxHeight
-                        TikTokLikeScreen(
+                        ScrollablePageScreen(
                             obj = objects[page],
                             onClick = { },
                             modifier = Modifier
@@ -78,13 +87,59 @@ data object ListScreen : Screen {
                         contentColor = Color.White,
                         elevation = 0.dp
                     ) {
-                        Row {
-                            Spacer(modifier = Modifier.weight(1.0F))
-                            Icon(
-                                imageVector = Icons.Default.Search,
-                                contentDescription = "Search"
-                            )
+                        var selectedTabIndex = 1
+                        Box(
+                            modifier = Modifier.fillMaxWidth()
+                                .align(alignment = Alignment.CenterVertically)
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                TabRow(
+                                    selectedTabIndex = selectedTabIndex,
+                                    modifier = Modifier.width(150.dp),
+                                    backgroundColor = Color.Transparent,
+                                    contentColor = Color.White
+                                ) {
+                                    Tab(
+                                        selected = selectedTabIndex == 0,
+                                        onClick = { /* Handle click */ },
+                                        text = { Text("Following") }
+                                    )
+                                    Tab(
+                                        selected = selectedTabIndex == 1,
+                                        onClick = { /* Handle click */ },
+                                        text = { Text("For You") }
+                                    )
+                                }
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(horizontal = 12.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+
+                                Row() {
+                                    Icon(
+                                        imageVector = Icons.Default.Person, // Replace with your actual clock icon
+                                        contentDescription = "Time",
+                                        modifier = Modifier.padding(end = 8.dp)
+                                    )
+                                    Text(text = "10m")
+                                }
+
+                                Icon(
+                                    imageVector = Icons.Default.Search,
+                                    contentDescription = "Search",
+                                )
+                            }
                         }
+
                     }
 
                     Spacer(
